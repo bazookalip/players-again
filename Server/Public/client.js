3c579f5f2330e16cda856d71c0aef1014356f3e9
Marcus Anderberg <marcusanderberg@gmail.com>
2019-02-07 18:46:40 -0600

    added server side POSt

--- Server/Public/client.js
+++ Server/Public/client.js
@@ -3,14 +3,44 @@ $(document).ready(onReady);
 
 function onReady(){
     console.log('jQuery is ready');
-    
-    $('#addPlayerButton').on('click', function (){
-        let player = $('#playerName').val();
-        $('#ulNames').append(`
-            <ul id="ulNames"></ul>
-                <li>${player}</li>
-        `)
-        $('#playerName').val('');
-    });
 
+
+    $.ajax({
+        url: '/players',
+        method: 'GET'
+    }).then(function(response) {
+        console.log(response);
+        $('#ulNames').empty();
+        response.forEach((playerObject) => {
+            $('#ulNames').append(`
+             <li>${playerObject}</li>  
+        `)
+        });   
+    })
+    
+    $('#addPlayerButton').on('click', addClick);
+}
+
+
+function addClick(){
+    // let player = $('#playerName').val();
+    $.ajax({
+        url: '/new',
+        method: 'POST',
+        data: {
+            player: $('#playerName').val()
+        }
+    }).then(function(){
+        $.ajax({
+            url: '/players',
+            method: 'GET'
+        }).then(function (response) {
+            console.log(response);
+            response.forEach((playerObject) => {
+                $('#ulNames').append(`
+             <li>${playerObject}</li>  
+        `)
+            });
+        })
+    })
 }
--- Server/server.js
+++ Server/server.js
@@ -2,17 +2,27 @@ let PORT = 5000;
 
 let express = require('express');
 
+let bodyParser = require('body-parser');
+
 let app = express();
 
 app.use(express.static('server/public'));
 
-let players = ['Larry', 'Joe']
+app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', (req, res) =>{
+let players = ['Larry', 'Joe', 'Alan']
+
+app.get('/players', (req, res) =>{
     res.send(players);
 })
 
+app.post('/new', (req, res) => {
+    players.push(req.body);
+    res.sendStatus(201);
+
+});
+
+
 app.listen(PORT, () => {
-    console.log('running on port', PORT);
-    
+    console.log('running on port', PORT);   
 })
